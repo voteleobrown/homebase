3fbc1bcbe5d369a9784534a2c5a5100aad33d019
Leo Brown <spam@leobrown.net>
2019-03-15 04:46:31 -0600

    Install options in install script.

--- install.sh
+++ install.sh
@@ -5,4 +5,25 @@ cp -r .vim $HOME
 cp -r .vimrc $HOME
 cp -r .zsh $HOME
 cp -r .zshrc $HOME
+touch .zsh/local.zsh
 
+read -n1 -p "Install vundle plugins for vim? [y,n]" vundleresp 
+case $vundleresp in  
+	y|Y) echo "Installing Vundle plugins..." && ./scripts/install_vundle.sh ;; 
+	n|N) echo "Vundle plugins not installed." ;; 
+	*) echo "Vundle plugins not installed" ;; 
+esac
+
+read -n1 -p "Install Node? [y,n]" npmresp 
+case $npmresp in  
+	y|Y) echo "Installing Node..." && ./scripts/install_nvm.sh ;; 
+	n|N) echo "Node not installed." ;; 
+	*) echo "Node not installed" ;; 
+esac
+
+read -n1 -p "Install Python 3 [y,n]" doit 
+case $doit in  
+	y|Y) echo "Installing Python 3..." && ./scripts/install_python3.sh ;; 
+	n|N) echo "Python 3 not installed." ;; 
+	*) echo "Python 3 not installed" ;; 
+esac
