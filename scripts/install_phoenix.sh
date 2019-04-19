a1c199ba6882e5aa238869ba57ccdf7287bfd8f8
Leo Brown <spam@leobrown.net>
2019-04-19 20:16:35 +0000

    Update 'install_phoenix.sh' to use Phoenix 1.4

--- scripts/install_phoenix.sh
+++ scripts/install_phoenix.sh
@@ -2,6 +2,6 @@ wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo 
 sudo apt-get update
 sudo apt-get -y install esl-erlang
 sudo apt-get -y install elixir
-mix archive.install https://github.com/phoenixframework/archives/raw/master/phx_new.ez
+mix archive.install hex phx_new 1.4.0
 sudo apt-get -y install inotify-tools
 rm erlang*.deb*
