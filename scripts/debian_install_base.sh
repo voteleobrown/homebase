4e20435c4454ac489926c05247fcfff386cb29ef
Leo Brown <spam@leobrown.net>
2020-01-09 20:01:35 -0700

    Forgot to re-add ddate

--- scripts/debian_install_base.sh
+++ scripts/debian_install_base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get install zsh vim tmux build-essential net-tools automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
+sudo apt-get install zsh vim tmux ddate build-essential net-tools automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 
 echo -e '\n. $HOME/.asdf/asdf.sh' >> ~/.zsh/local.zsh
