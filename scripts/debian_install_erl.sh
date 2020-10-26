59e7d8a72cd39f3e7377ba3a4c2a1c80c163734b
Leo <spam@leobrown.net>
2020-10-25 21:46:18 -0600

    update asdf

--- scripts/debian_install_erl.sh
+++ scripts/debian_install_erl.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 sudo apt-get -y install build-essential autoconf m4 libncurses5-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop inotify-tools
 
-asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
-asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
-
-asdf install erlang 22.1.3
-asdf global erlang 22.1.3
-asdf install elixir 1.9.1-otp-22
-asdf global elixir 1.9.1-otp-22
+asdf install erlang 23.1.1
+asdf global erlang 23.1.1
+asdf install elixir 1.11.1-otp-23
+asdf global elixir 1.11.1-otp-23
+asdf install rebar 3.14.0
+asdf global rebar 3.14.0
 
 mix local.hex
 mix archive.install https://github.com/phoenixframework/archives/raw/master/phoenix_new.ez
