2e803c6bbfe7366ced187f7c7830f3d89ef6b336
Leo <spam@leobrown.net>
2020-10-30 15:26:10 -0600

    better debian detection

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ case "$OSTYPE" in
   darwin*)  echo "OSX" ;; 
   linux-android) ./scripts/install_termux.sh ;;
   linux*)
-	  ID_LIKE=$(cat /etc/os-release | grep ID_LIKE)
-	  case "$ID_LIKE" in
+	  ID=$(cat /etc/os-release | grep ID)
+	  case "$ID" in
 		  *debian*)    echo "Debian-based distributrion detected..." && ./scripts/install_debian.sh ;;
 		  *fedora*)    echo "Fedora-like OS";;
 		  *)           echo "unknown: $ID_LIKE";;
