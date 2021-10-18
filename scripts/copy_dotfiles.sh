8798cf4d29d7140780174cdca57eec4cb23423f3
Leo Brown <leo@meowwolf.com>
2021-10-18 12:52:19 -0600

    update dot file installer

--- scripts/copy_dotfiles.sh
+++ scripts/copy_dotfiles.sh
@@ -6,3 +6,10 @@ cp -r .vimrc $HOME
 cp -r .zsh $HOME
 cp -r .zshrc $HOME
 touch .zsh/local.zsh
+
+# prezto
+cp ./.zlogin $HOME
+cp ./.zlogout $HOME
+cp ./.zpreztorc $HOME
+cp ./.zshenv $HOME
+cp ./.p10k.zsh $HOME 
